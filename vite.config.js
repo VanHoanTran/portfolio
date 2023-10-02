111871d740f5f17167d6ff1baadeb4b8aab6b122
vanhoantran <vanhoantranca@gmail.com>
2023-10-02 13:42:29 -0400

    try

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 /* eslint-disable no-undef */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+//import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/portfolio/",
   plugins: [react()],
   resolve: {
-    alias: {
-      // Define your aliases using path.resolve()
-      "/src": path.resolve(__dirname, "src"),
-    },
+    preserveSymlinks: true,
+    // alias: {
+    //   // Define your aliases using path.resolve()
+    //   "/src": path.resolve(__dirname, "src"),
+    // },
   },
 });
