9b3ab0ea80b76a3ad6ce36672dac14c1523737f9
vanhoantran <vanhoantranca@gmail.com>
2023-10-02 15:20:33 -0400

    fix import path

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,27 @@
 /* eslint-disable no-undef */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 //import path from "path";
 
 // https://vitejs.dev/config/
+
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, dist);
 export default defineConfig({
   plugins: [react()],
+  root,
   server: {
     port: 8080,
     open: true,
   },
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(__dirname, "index.html"),
-  //       src: resolve(__dirname, "nested/index.html"),
-  //     },
-  //   },
-  // },
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
