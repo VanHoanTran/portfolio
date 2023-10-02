b3d4c3f97d773a897a9c94655a9ade62edf9396e
vanhoantran <vanhoantranca@gmail.com>
2023-10-02 11:08:12 -0400

    test path name

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +8,10 @@ export default defineConfig({
   resolve: {
     alias: {
       // Define your aliases here
-      "@components": "/src/components",
-      "@data": "src/data",
+      // eslint-disable-next-line no-undef
+      "@components": path.resolve(__dirname, "src/components"),
+      // eslint-disable-next-line no-undef
+      "@data": path.resolve(__dirname, "src/data"),
     },
   },
 });
