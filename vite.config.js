f34fbf24699a9db7244785a9b2bec7b8e0a9dac6
vanhoantran <vanhoantranca@gmail.com>
2023-10-02 12:34:03 -0400

    add alias

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+/* eslint-disable no-undef */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Define your aliases using path.resolve()
+      "/src": path.resolve(__dirname, "src"),
+    },
+  },
 });
