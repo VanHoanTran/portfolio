fe57e211c4a0a0b207665dc1ef0a09f849dfe3c8
vanhoantran <vanhoantranca@gmail.com>
2023-10-02 11:05:52 -0400

    test path name

--- src/components/Education/index.jsx
+++ src/components/Education/index.jsx
@@ -1,6 +1,6 @@
-import { edu } from "@data/edu";
+import { edu } from "@data/edu.js";
 import SkillSet from "./SkillSet";
-import { skills } from "@data/skills";
+import { skills } from "@data/skills.js";
 const Education = () => {
   return (
     <section
