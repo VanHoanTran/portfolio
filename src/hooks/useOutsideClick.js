9d138ea5c960387e515d9605c9a14e4ace4c0369
vanhoantran <vanhoantranca@gmail.com>
2023-10-03 09:40:21 -0400

    add comment on the function

--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -1,5 +1,8 @@
 import { useEffect, useRef } from "react";
 
+// capture click event
+// if a click occurs outside of element which assign to ref from useRef.
+// then execute the handler func. Otherwise, do nothing
 const useOutsideClick = (handler, listenCapturing = true) => {
   const ref = useRef();
   useEffect(() => {
